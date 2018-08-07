b172d54434479f0c41c5e7c9abe3d5f4d602914b
toshi0607 <s.toshi0607@gmail.com>
2018-08-07 23:06:50 +0900

    add skelton

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(s3Event events.S3Event) error {
+	return nil
+}
