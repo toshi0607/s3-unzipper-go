55f47ae60ba7eb51b54469ac2c5a0f991eb5aad2
toshi0607 <s.toshi0607@gmail.com>
2018-08-12 16:07:45 +0900

    :recycle: decouple unzip

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"archive/zip"
 	"context"
-	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -18,6 +16,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/toshi0607/unzipper/zip"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -80,7 +79,7 @@ func handler(ctx context.Context, s3Event events.S3Event) error {
 	}
 	log.Println("Downloaded", file.Name(), numBytes, "bytes")
 
-	err = Unzip(zipContentPath+tempZip, unzipContentPath)
+	err = zip.Unzip(zipContentPath+tempZip, unzipContentPath)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -140,41 +139,6 @@ func prepareDirectory() error {
 	}
 	if err := os.MkdirAll(unzipContentPath, 0777); err != nil {
 		return err
-	}
-
-	return nil
-}
-
-func Unzip(src, dest string) error {
-	r, err := zip.OpenReader(src)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	for _, f := range r.File {
-		rc, err := f.Open()
-		if err != nil {
-			return err
-		}
-		defer rc.Close()
-
-		path := filepath.Join(dest, f.Name)
-		if f.FileInfo().IsDir() {
-			os.MkdirAll(path, f.Mode())
-		} else {
-			f, err := os.OpenFile(
-				path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
-			if err != nil {
-				return err
-			}
-			defer f.Close()
-
-			_, err = io.Copy(f, rc)
-			if err != nil {
-				return err
-			}
-		}
 	}
 
 	return nil
